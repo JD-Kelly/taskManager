2aecdaa96a225b78cd28fd55b9dd339e5cb2e2f1
Jonny Kelly <jonny.kelly192@gmail.com>
2021-11-04 18:06:04 +0000

    can update a task

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -32,9 +32,24 @@ const getTask = async (req, res) => {
   
 }
 
-const updateTask = (req, res) => {
-  res.send('update task')
+const updateTask = async (req, res) => {
+  try {
+    const { id:taskID } = req.params;
+    const task = await Task.findOneAndUpdate({_id:taskID}, req.body, {
+      new: true,
+      runValidators: true
+    })
+    if(!task){
+      return res.status(404).json({msg: `no task with id: ${taskID}`})
+    }
+    res.status(200).json({})
+  } catch (error) {
+    res.status(500).json({msg: error })
+  }
 }
+
+
+
 
 const deleteTask = async (req, res) => {
   try {
