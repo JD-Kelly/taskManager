a5c80ada590c3f8df216656029e26cdfbfb017a1
Jonny Kelly <jonny.kelly192@gmail.com>
2021-11-04 18:14:04 +0000

    bug fix with editing a task

--- app.js
+++ app.js
@@ -5,16 +5,11 @@ const tasks = require('./routes/tasks')
 const connectDB = require('./db/connect')
 require('dotenv').config()
 
+app.use(express.static('./public'))
 app.use(express.json())
 
 
-
-app.get('/hello', (req, res) => {
-  res.send("Task Manager App")
-})
-
 app.use('/api/v1/tasks', tasks)
-
 
 
 const start = async () => {
--- controllers/tasks.js
+++ controllers/tasks.js
@@ -42,14 +42,11 @@ const updateTask = async (req, res) => {
     if(!task){
       return res.status(404).json({msg: `no task with id: ${taskID}`})
     }
-    res.status(200).json({})
+    res.status(200).json({ task })
   } catch (error) {
     res.status(500).json({msg: error })
   }
 }
-
-
-
 
 const deleteTask = async (req, res) => {
   try {
