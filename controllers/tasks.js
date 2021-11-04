0e122bc3ca8c0771f29f06b5542361a60eeb5272
Jonny Kelly <jonny.kelly192@gmail.com>
2021-11-04 12:30:22 +0000

    initial set-up of all task routes

---
+++ controllers/tasks.js
@@ -0,0 +1,30 @@
+const getAllTasks = (req, res) => {
+  res.send('get all tasks')
+}
+
+const createTask = (req, res) => {
+  res.send('create new task')
+}
+
+const getTask = (req, res) => {
+  res.send('get single tasks')
+}
+
+const updateTask = (req, res) => {
+  res.send('update task')
+}
+
+const deleteTask = (req, res) => {
+  res.send('delete task')
+}
+
+
+
+
+module.exports = {
+  getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask
+}
